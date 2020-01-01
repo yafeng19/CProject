ca6b1f7bff77b872e28aaa354c2e9945e7c20265
yafeng19 <15893009558@qq.com>
2020-01-01 19:39:37 +0800

    fix FORMAT

--- 0最大公约数/main.c
+++ 0最大公约数/main.c
@@ -3,29 +3,31 @@
 int Gcd(int a, int b);
 int main()
 {
-    int m,n,result;
+    int m, n, result;
     printf("Input m,n:");
-    scanf("%d,%d",&m,&n);
-    result=Gcd(m,n);
-    if (result==-1)printf("Input error!\n");
+    scanf("%d,%d", &m, &n);
+    result = Gcd(m, n);
+    if (result == -1)
+        printf("Input error!\n");
     return 0;
 }
 int Gcd(int a, int b)
 {
-    int i,max=1;
-    if (a>=1&&a<=10000&&b>=1&&b<=10000)
+    int i, max = 1;
+    if (a >= 1 && a <= 10000 && b >= 1 && b <= 10000)
     {
-        for(i=1;i<=a||i<=b;i++)
+        for (i = 1; i <= a || i <= b; i++)
         {
-            if(a%i==0&&b%i==0)
+            if (a % i == 0 && b % i == 0)
             {
-                max*=i;
-                a/=i;
-                b/=i;
+                max *= i;
+                a /= i;
+                b /= i;
             }
         }
-        printf("%d/%d\n",a,b);
-        printf("%d",max);
+        printf("%d/%d\n", a, b);
+        printf("%d", max);
     }
-    else return -1;
+    else
+        return -1;
 }
--- 1链表排序/main.c
+++ 1链表排序/main.c
@@ -1,16 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
+struct node
 {
-    union bt
+    int val;
+    struct node *next;
+};
+
+struct node *head;
+struct node *cur;
+void create(struct node *head, int i)
+{
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    cur->next = new;
+    new->val = i;
+    cur = cur->next;
+}
+
+void display(struct node *head, int n)
+{
+    struct node *p = head->next;
+    for (int i = 0; i < n; i++)
     {
-        int nk;
-        char cc[4];
-    }a;
-    a.nk=-17;
-    printf("%d\n",a.cc[0]);
-    printf("%d\n",a.cc[1]);
-    printf("%d\n",a.cc[2]);
-    printf("%d\n",a.cc[3]);
+        printf(" %d", p->val);
+        p = p->next;
+    }
+}
+
+struct node* sort(struct node* head)
+{
+    for(struct node* i = head->next; i != NULL; i = i->next) {
+        for(struct node* j = head->next; j != NULL; j = j->next) {
+            if(j->val > i->val) {
+                int tmp = i->val;
+                i->val = j->val;
+                j->val = tmp;
+            }
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    head = (struct node *)malloc(sizeof(struct node));
+    cur = head;
+    int n, i;
+    scanf("%d", &n);
+    int tmp = n;
+    while (tmp--)
+    {
+        scanf("%d", &i);
+        create(head, i);
+    }
+    cur->next = NULL;
+    display(head, n);
+    head = sort(head);
+    printf("\n");
+    display(head, n);
 }
